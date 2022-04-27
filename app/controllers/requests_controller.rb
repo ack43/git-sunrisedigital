caaba19e88b3ec61db049e4e1420b88d70a62f6b
Alexander Kiseliev <i43ack@gmail.com>
2022-04-27 11:38:21 +0300

    request empty upd

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,18 +1,23 @@
 class RequestsController < ApplicationController
   def submit_form
     @request = Request.new(params[:request])
-    RequestNotifier.received(@request.message).deliver
-    flash[:success] = "Ваша заявка принята. Менеджер свяжется с вами в ближайшее время!"
+    validator(@request)
+    unless @errors
+      RequestNotifier.received(@request.message).deliver
+      flash[:success] = "Ваша заявка принята. Менеджер свяжется с вами в ближайшее время!"
+    else
+      flash[:error] = "В заявке обнаружены ошибки. Введите данные заново."
+    end
     
     respond_to do |format|
-      format.html { redirect_to root_url(submit: "true", type: params[:request][:type].downcase) }
+      format.html { redirect_to root_url(submit: @errors.blank?, type: params[:request][:type].downcase) }
       format.js
     end
   end
   
   def validate
-    request = Request.new(params[:request])
-    validator(request)
+    @request = Request.new(params[:request])
+    validator(@request)
     respond_to do |format|
       format.json { render json: @errors }
     end
