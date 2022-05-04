50b20e6d5627a59af5845fc800240b49957da4f8
Alexander Kiseliev <i43ack@gmail.com>
2022-05-04 09:17:45 +0300

    RequestNotifier error

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -2,8 +2,21 @@ class RequestsController < ApplicationController
   def submit_form
     @request = Request.new(request_params)
     validator(@request)
-    unless @errors
-      RequestNotifier.received(@request.message).deliver
+    if @errors == true || @errors.empty?
+      begin
+        RequestNotifier.received(@request.message).deliver
+      rescue Exception => ex
+        Rails.logger.error ''
+        Rails.logger.error 'RequestNotifier error:'
+        Rails.logger.error "Cannot send e-mail with request"
+        Rails.logger.error "Request:"
+        Rails.logger.error @request.inspect
+        Rails.logger.error "Exception:"
+        Rails.logger.error ex.message
+        Rails.logger.error ex.inspect
+        Rails.logger.error ''
+        Rails.logger.error ''
+      end
       flash[:success] = "Ваша заявка принята. Менеджер свяжется с вами в ближайшее время!"
     else
       flash[:error] = "В заявке обнаружены ошибки. Введите данные заново."
